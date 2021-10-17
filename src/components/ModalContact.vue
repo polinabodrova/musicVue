42af92b197ca623d52c3d3f75a869a289bf69f54
Andrei <1328bas@gmail.com>
2021-10-17 07:58:53 +0300

    basic modal window logic added

---
+++ src/components/ModalContact.vue
@@ -0,0 +1,23 @@
+git pull
+<template>
+  <modal name="my-first-modal">
+    <ContactForm />
+  </modal>
+</template>
+
+<script>
+import ContactForm from "../components/ContactForm.vue";
+export default {
+  name: "MyComponent",
+  components: { ContactForm },
+  methods: {
+    show() {
+      this.$modal.show("my-first-modal");
+    },
+    hide() {
+      this.$modal.hide("my-first-modal");
+    },
+  },
+  mount() {},
+};
+</script>
--- src/views/Pricing.vue
+++ src/views/Pricing.vue
@@ -2,24 +2,46 @@
   <section id="pricing">
     <logo-section />
     <div class="main">
+      <ModalContact ref="modalRef" />
       <div class="price__container">
-        <div class="price__container-item">
+        <div class="price__container-item" @click="show">
           <div class="price__container-item-head">30 $ BASIC</div>
-          <div class="price__container-item-main"></div>
+          <div class="price__container-item-main">
+            Simple demo beat. Up to 2 minutes. Non exclusive license.
+          </div>
         </div>
-        <div class="price__container-item">
+        <div class="price__container-item" @click="show">
           <div class="price__container-item-head">40 $ STANDARD</div>
-          <div class="price__container-item-main"></div>
+          <div class="price__container-item-main">
+            Custom beat up to 4 minutes. Exclusive license.
+          </div>
         </div>
-        <div class="price__container-item">
+        <div class="price__container-item" @click="show">
           <div class="price__container-item-head">50 $ PREMIUM</div>
-          <div class="price__container-item-main"></div>
+          <div class="price__container-item-main">
+            Custom beat up to 4 minutes. Exclusive license + Stems.
+          </div>
         </div>
       </div>
     </div>
   </section>
 </template>
-<script></script>
+<script>
+import ModalContact from "../components/ModalContact.vue";
+export default {
+  name: "Pricing",
+  components: { ModalContact },
+  methods: {
+    show() {
+      this.$refs.modalRef.show();
+    },
+    hide() {
+      this.$refs.modalRef.hide();
+    },
+  },
+  mount() {},
+};
+</script>
 <style scoped lang="scss">
 $main: #393e46;
 $greenblue: #00adb5;
